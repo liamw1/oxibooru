2a9ec80c5c72d4ba9eac912b7c164ca080340816
liamw1 <liamw1@vt.edu>
2024-07-28 19:22:59 -0400

    Updated comments and added asserts.

--- server/src/resource/mod.rs
+++ server/src/resource/mod.rs
@@ -14,15 +14,25 @@ fn check_batch_results(batch_size: usize, post_count: usize) {
 }
 
 /*
-    This algorithm is O(n^2) in post_ids.len(), which could be made O(n) with a HashMap implementation.
+    For a given set of resources, orders them so that their primary keys are in the same order
+    as the order slice, which should be the same length as values.
+
+    NOTE:
+    This algorithm is O(n^2) in values.len(), which could be made O(n) with a HashMap implementation.
     However, for small n this Vec-based implementation is probably much faster. Since we retrieve
-    40-50 posts at a time, I'm leaving it like this for the time being until it proves to be slow.
+    40-50 resources at a time, I'm leaving it like this for the time being until it proves to be slow.
 */
-fn order_by<T: IntegerIdentifiable>(mut values: Vec<T>, order: &[i32]) -> Vec<T> {
+fn order_by<T>(mut values: Vec<T>, order: &[i32]) -> Vec<T>
+where
+    T: IntegerIdentifiable,
+{
+    assert_eq!(values.len(), order.len());
+
     let mut index = 0;
     while index < order.len() {
         let value_id = values[index].id();
         let correct_index = order.iter().position(|&id| id == value_id).unwrap();
+        assert!(correct_index >= index, "Value id is not unique");
         if index != correct_index {
             values.swap(index, correct_index);
         } else {
@@ -32,6 +42,13 @@ fn order_by<T: IntegerIdentifiable>(mut values: Vec<T>, order: &[i32]) -> Vec<T>
     values
 }
 
+/*
+    Maps a set of resources to a Vec of Options that contains these resources ordered according
+    to the primary keys of ordered_values. If there are less unorderd_values than ordered_values,
+    the missing values are padded as None in the resulting vector.
+
+    The note in the above comment applies here as well.
+*/
 fn order_as<V, T, F>(unordered_values: Vec<V>, ordered_values: &[T], get_key: F) -> Vec<Option<V>>
 where
     T: IntegerIdentifiable,
