6e172a251c97ff8f6ce4c21a573602e06da3be61
liamw1 <liamw1@vt.edu>
2024-08-19 20:11:57 -0400

    Set image reader allocation limit to 4GB.

--- server/src/image/read.rs
+++ server/src/image/read.rs
@@ -1,10 +1,18 @@
-use image::ImageReader;
+use image::{ImageReader, Limits};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
 pub fn new_image_reader(image_path: &Path) -> std::io::Result<ImageReader<BufReader<File>>> {
     let mut reader = image::ImageReader::open(image_path)?;
-    reader.no_limits(); // TODO: Set reasonable limits
+    reader.limits(image_reader_limits());
     Ok(reader)
+}
+
+fn image_reader_limits() -> Limits {
+    const GB: u64 = 1024 * 1024 * 1024;
+
+    let mut limits = Limits::no_limits();
+    limits.max_alloc = Some(4 * GB);
+    limits
 }
