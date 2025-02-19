bcff32b8c54811134dd3e0bb5262b089d6040092
liamw1 <liamw1@vt.edu>
2025-02-19 17:28:13 -0500

    Added fake user-agent to downloader to prevent errors on some websites when retrieving content via URL.

--- server/src/content/download.rs
+++ server/src/content/download.rs
@@ -1,6 +1,7 @@
 use crate::api::ApiResult;
 use crate::model::enums::MimeType;
 use crate::{api, filesystem};
+use reqwest::Client;
 use std::str::FromStr;
 use url::Url;
 
@@ -8,7 +9,9 @@ use url::Url;
 /// If successful, the file is saved in the temporary uploads directory
 /// and a content token is returned.
 pub async fn from_url(url: Url) -> ApiResult<String> {
-    let response = reqwest::get(url).await?;
+    const FAKE_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0";
+    let client = Client::builder().user_agent(FAKE_USER_AGENT).build()?;
+    let response = client.get(url).send().await?;
     let response = response.error_for_status()?;
 
     let content_type = response
