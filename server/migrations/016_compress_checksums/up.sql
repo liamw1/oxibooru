a5e7313fbe2b7c0fca88661d940a1008be1cc30a
liamw1 <liamw1@vt.edu>
2025-03-13 00:40:23 -0400

    Fixed migration issues for converted databases.
    Previously, the up.sql script would assume that every row was a valid base64 sequence and would error if a single invalid character was found. However, for converted databases this isn't always correct, checksums are randomized in the conversion script. These "invalid" checksums may persist even after running `recompute_post_checksums` because there may be rows which no longer correspond to a file on disk.
    
    I made the migration more flexible by adding a fallback that converts TEXT directly to binary.

--- server/migrations/016_compress_checksums/up.sql
+++ server/migrations/016_compress_checksums/up.sql
@@ -7,11 +7,23 @@ WHERE "checksum_md5" IS NULL;
 
 ALTER TABLE "post" ENABLE TRIGGER USER;
 
+-- Decodes from base64 if it is a valid base64 sequence. Converts to UTF8 binary otherwise.
+CREATE FUNCTION try_decode_base64(input TEXT) RETURNS BYTEA AS $$
+BEGIN
+    -- PostgreSQL expects a padded base64 sequence
+    RETURN decode(input || REPEAT('=', 4 - LENGTH("checksum") % 4), 'base64');
+EXCEPTION WHEN others THEN
+    RETURN convert_to(input, 'UTF8');
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
 ALTER TABLE "post"
 ALTER COLUMN "checksum" TYPE BYTEA
-USING decode("checksum" || REPEAT('=', 4 - LENGTH("checksum") % 4), 'base64');
+USING try_decode_base64("checksum"); 
 
 ALTER TABLE "post"
 ALTER COLUMN "checksum_md5" TYPE BYTEA
-USING decode("checksum_md5" || REPEAT('=', 4 - LENGTH("checksum_md5") % 4), 'base64'),
+USING try_decode_base64("checksum_md5"),
 ALTER COLUMN "checksum_md5" SET NOT NULL;
+
+DROP FUNCTION try_decode_base64;
