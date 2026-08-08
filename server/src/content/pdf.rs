37f7111ccff7598ac36a6d884a198e7ff37a6e44
EPICGameGuy <admin@cactus.vg>
2026-08-07 20:48:49 -0400

    Got PDFs working

--- server/src/content/pdf.rs
+++ server/src/content/pdf.rs
@@ -1,19 +1,11 @@
 use crate::api::error::{ApiError, ApiResult};
-use crate::content::{self, flash};
-use crate::model::enums::{MimeType, PostType};
 use hayro::hayro_interpret::InterpreterSettings;
-use hayro::hayro_interpret::font::{FontData, FontQuery, StandardFont};
-use hayro::hayro_interpret::hayro_cmap::CidFamily;
 use hayro::hayro_syntax::Pdf;
 use hayro::{RenderCache, RenderSettings, render};
 use image::{DynamicImage, RgbaImage};
-use std::borrow::Cow;
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::Read;
 use std::path::Path;
-use std::str::FromStr;
-use std::sync::Arc;
-use std::sync::mpsc::{RecvTimeoutError, SyncSender};
 use vello_cpu::color::palette::css::WHITE;
 
 pub fn pdf_representative_image(file_path: &Path) -> ApiResult<DynamicImage> {
@@ -26,22 +18,37 @@ pub fn pdf_representative_image(file_path: &Path) -> ApiResult<DynamicImage> {
 
     let interpreter_settings = InterpreterSettings { ..Default::default() };
 
+    let page = pdf
+        .pages()
+        .get(0)
+        .ok_or(ApiError::FromStr("Failed to get page 1 from PDF".into()))?;
+
+    let (dimensions, ratio) = {
+        let dimensions = page.render_dimensions();
+
+        let max_size = 1000.0;
+
+        if dimensions.0 <= max_size && dimensions.1 <= max_size {
+            (dimensions, 1.0)
+        } else {
+            let longest_side = dimensions.0.max(dimensions.1);
+            let ratio = max_size / longest_side;
+
+            ((dimensions.0 * ratio, dimensions.1 * ratio), ratio)
+        }
+    };
+
     let render_settings = RenderSettings {
-        x_scale: 1.0,
-        y_scale: 1.0,
+        x_scale: ratio,
+        y_scale: ratio,
+        width: Some(dimensions.0 as u16),
+        height: Some(dimensions.1 as u16),
         bg_color: WHITE,
         ..Default::default()
     };
     let cache = RenderCache::new();
 
-    let pixmap = render(
-        pdf.pages()
-            .get(0)
-            .ok_or(ApiError::FromStr("Failed to get page 1 from PDF".into()))?,
-        &cache,
-        &interpreter_settings,
-        &render_settings,
-    );
+    let pixmap = render(page, &cache, &interpreter_settings, &render_settings);
 
     let png = pixmap.data_as_u8_slice();
 
@@ -49,9 +56,4 @@ pub fn pdf_representative_image(file_path: &Path) -> ApiResult<DynamicImage> {
         RgbaImage::from_raw(pixmap.width() as u32, pixmap.height() as u32, png.to_vec())
             .ok_or(ApiError::FromStr("Failed to render PDF".into()))?,
     ))
-
-    // let mut reader = ImageReader::new(BufReader::new(png.as_slice()));
-    // reader.set_format(ImageFormat::Png);
-    // reader.limits(Limits::default());
-    // reader.decode().map_err(ApiError::from)
 }
