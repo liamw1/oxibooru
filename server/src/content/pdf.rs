2f242b315f76402e7a85e660df670caa5dfaba98
EPICGameGuy <admin@cactus.vg>
2026-08-09 19:23:32 -0400

    Moved PDF render dimensions into separate struct, functions

--- server/src/content/pdf.rs
+++ server/src/content/pdf.rs
@@ -3,11 +3,57 @@ use crate::config::Config;
 use crate::content;
 use hayro::hayro_interpret::InterpreterSettings;
 use hayro::hayro_syntax::Pdf;
+use hayro::hayro_syntax::page::Page;
 use hayro::vello_cpu::color::palette::css::WHITE;
+use hayro::vello_cpu::color::{AlphaColor, Srgb};
 use hayro::{RenderCache, RenderSettings, render};
 use image::error::LimitErrorKind;
 use image::{DynamicImage, RgbaImage};
 use std::path::Path;
+
+struct PdfRenderDimensions {
+    pub width: f32,
+    pub height: f32,
+    pub ratio: f32,
+}
+
+impl PdfRenderDimensions {
+    // Calculates the dimensions to render a PDF page at, given the config limits and the page's original dimensions.
+    fn from_page(config: &Config, page: &Page<'_>) -> Self {
+        let (width, height) = page.render_dimensions();
+
+        // find the min ratio to scale down the image while maintaining aspect ratio
+        let ratio = {
+            let max_width = f32::from(config.limits.max_pdf_width);
+            let max_height = f32::from(config.limits.max_pdf_height);
+
+            let width_ratio = max_width / width;
+            let height_ratio = max_height / height;
+
+            f32::min(width_ratio, height_ratio)
+        };
+
+        // ensure ratio is at most 1.0. We only want to downscale, not upscale.
+        let ratio = f32::min(1.0, ratio);
+
+        Self {
+            width: width * ratio,
+            height: height * ratio,
+            ratio: ratio,
+        }
+    }
+
+    // Returns the render settings for rendering a PDF page at the dimensions specified by this struct.
+    fn render_settings(&self, background_color: AlphaColor<Srgb>) -> ApiResult<RenderSettings> {
+        Ok(RenderSettings {
+            x_scale: self.ratio,
+            y_scale: self.ratio,
+            width: Some(num_traits::cast(self.width).ok_or(LimitErrorKind::DimensionError)?),
+            height: Some(num_traits::cast(self.height).ok_or(LimitErrorKind::DimensionError)?),
+            bg_color: background_color,
+        })
+    }
+}
 
 pub fn pdf_preview_image(config: &Config, file_path: &Path) -> ApiResult<DynamicImage> {
     let pdf = {
@@ -15,36 +61,14 @@ pub fn pdf_preview_image(config: &Config, file_path: &Path) -> ApiResult<Dynamic
         Pdf::new(file_contents).map_err(crate::model::enums::PdfLoadError)?
     };
 
+    // The preview image is just gonna show the first page
     let page = pdf.pages().first().ok_or(ApiError::EmptyPdf)?;
 
-    let (page_width, page_height, ratio) = {
-        let dimensions = page.render_dimensions();
+    let dimensions = PdfRenderDimensions::from_page(config, page);
 
-        let max_size = (f32::from(config.limits.max_pdf_width), f32::from(config.limits.max_pdf_height));
+    let pixmap =
+        render(page, &RenderCache::new(), &InterpreterSettings::default(), &dimensions.render_settings(WHITE)?);
 
-        let ratios = (max_size.0 / dimensions.0, max_size.1 / dimensions.1);
-
-        // find the min ratio to scale down the image while maintaining aspect ratio
-        let ratio = f32::min(ratios.0, ratios.1);
-
-        // ensure ratio is at most 1.0. We only want to downscale, not upscale.
-        let ratio = f32::min(1.0, ratio);
-
-        (dimensions.0 * ratio, dimensions.1 * ratio, ratio)
-    };
-
-    let page_width = num_traits::cast(page_width).ok_or(LimitErrorKind::DimensionError)?;
-    let page_height = num_traits::cast(page_height).ok_or(LimitErrorKind::DimensionError)?;
-
-    let render_settings = RenderSettings {
-        x_scale: ratio,
-        y_scale: ratio,
-        width: Some(page_width),
-        height: Some(page_height),
-        bg_color: WHITE,
-    };
-
-    let pixmap = render(page, &RenderCache::new(), &InterpreterSettings::default(), &render_settings);
     let width = pixmap.width();
     let height = pixmap.height();
 
