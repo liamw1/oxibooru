cb20cc836c3d7695ff103981fe1801633020c2ff
EPICGameGuy <admin@cactus.vg>
2026-08-09 18:15:09 -0400

    Changed error types in pdf_preview_image

--- server/src/content/pdf.rs
+++ server/src/content/pdf.rs
@@ -3,10 +3,11 @@ use crate::config::Config;
 use crate::content;
 use hayro::hayro_interpret::InterpreterSettings;
 use hayro::hayro_syntax::Pdf;
+use hayro::vello_cpu::color::palette::css::WHITE;
 use hayro::{RenderCache, RenderSettings, render};
+use image::error::LimitErrorKind;
 use image::{DynamicImage, RgbaImage};
 use std::path::Path;
-use vello_cpu::color::palette::css::WHITE;
 
 pub fn pdf_preview_image(config: &Config, file_path: &Path) -> ApiResult<DynamicImage> {
     let pdf = {
@@ -16,7 +17,7 @@ pub fn pdf_preview_image(config: &Config, file_path: &Path) -> ApiResult<Dynamic
 
     let page = pdf.pages().first().ok_or(ApiError::EmptyPdf)?;
 
-    let (dimensions, ratio) = {
+    let (page_width, page_height, ratio) = {
         let dimensions = page.render_dimensions();
 
         let max_size = (f32::from(config.limits.max_pdf_width), f32::from(config.limits.max_pdf_height));
@@ -29,29 +30,40 @@ pub fn pdf_preview_image(config: &Config, file_path: &Path) -> ApiResult<Dynamic
         // ensure ratio is at most 1.0. We only want to downscale, not upscale.
         let ratio = f32::min(1.0, ratio);
 
-        ((dimensions.0 * ratio, dimensions.1 * ratio), ratio)
+        (dimensions.0 * ratio, dimensions.1 * ratio, ratio)
     };
 
-    let dimensions: (u16, u16) = (
-        num_traits::cast(dimensions.0).ok_or(ApiError::FromStr("Failed to render PDF".into()))?,
-        num_traits::cast(dimensions.1).ok_or(ApiError::FromStr("Failed to render PDF".into()))?,
-    );
+    let page_width = num_traits::cast(page_width).ok_or(LimitErrorKind::DimensionError)?;
+    let page_height = num_traits::cast(page_height).ok_or(LimitErrorKind::DimensionError)?;
 
     let render_settings = RenderSettings {
         x_scale: ratio,
         y_scale: ratio,
-        width: Some(dimensions.0),
-        height: Some(dimensions.1),
+        width: Some(page_width),
+        height: Some(page_height),
         bg_color: WHITE,
     };
-    let cache = RenderCache::new();
 
-    let pixmap = render(page, &cache, &InterpreterSettings::default(), &render_settings);
+    let pixmap = render(page, &RenderCache::new(), &InterpreterSettings::default(), &render_settings);
+    let width = pixmap.width();
+    let height = pixmap.height();
 
-    let png = pixmap.data_as_u8_slice();
+    // There should be some way to do this without reallocating,
+    // Vec<PreMulRgba8> is the same bitwise as Vec<u8> (except for num elements)
+    let rgba_vec = pixmap
+        .take()
+        .into_iter()
+        .map(|c| c.to_u8_array())
+        .flatten()
+        .collect::<Vec<u8>>();
+
+    let size = rgba_vec.len();
 
     Ok(DynamicImage::ImageRgba8(
-        RgbaImage::from_raw(u32::from(pixmap.width()), u32::from(pixmap.height()), png.to_vec())
-            .ok_or(ApiError::FromStr("Failed to render PDF".into()))?,
+        RgbaImage::from_raw(u32::from(width), u32::from(height), rgba_vec).ok_or(ApiError::FrameBufferMismatch(
+            width as u32,
+            height as u32,
+            size as usize,
+        ))?,
     ))
 }
