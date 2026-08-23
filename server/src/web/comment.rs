2cf8f61d1ed1c3e940372d4f4ad5a401ec897171
Liam Welch <liamw1@vt.edu>
2026-08-23 16:49:51 -0400

    wip: Fixed limits for post, tag, pool, and comment listing.

--- server/src/web/comment.rs
+++ server/src/web/comment.rs
@@ -1,6 +1,6 @@
 use crate::app::{AppState, Context};
 use crate::config::Action;
-use crate::extract::{Ctx, Json, PageParams, Query, ResourceParams};
+use crate::extract::{Ctx, Json, Offset, Query, ResourceParams};
 use crate::resource::post::{Field, PostInfo};
 use crate::web::Tab;
 use crate::web::pager::{Page, Pager};
@@ -8,10 +8,13 @@ use crate::{api, time};
 use askama::Template;
 use axum::response::Html;
 use axum::{Router, routing};
+use std::num::NonZeroU64;
 
 pub fn routes() -> Router<AppState> {
     Router::new().route("/comments", routing::get(list))
 }
+
+const LIMIT: NonZeroU64 = NonZeroU64::new(10).unwrap();
 
 #[derive(Template)]
 #[template(path = "pages/comments_page.html")]
@@ -22,11 +25,12 @@ struct ListTemplate<'a> {
     pager: Pager<'a, ()>,
 }
 
-async fn list(ctx: Ctx, page_params: Query<PageParams>) -> Html<String> {
+async fn list(ctx: Ctx, Query(offset): Query<Offset>) -> Html<String> {
     let fields = [Field::Id, Field::ThumbnailUrl, Field::Comments].into();
 
     let query = Some("sort:comment-date comment-count:1..".to_owned());
     let resource_params = Query(ResourceParams { query, fields });
+    let page_params = Query(offset.to_page_params(LIMIT));
     let Json(response) = api::post::list(ctx.clone(), resource_params, page_params)
         .await
         .unwrap();
