9d52e933d73bf0a6387465783d3979949083000a
Liam Welch <liamw1@vt.edu>
2026-08-23 23:26:03 -0400

    wip: Implemented read-only settings.

--- server/src/web/settings.rs
+++ server/src/web/settings.rs
@@ -10,18 +10,42 @@ pub fn routes() -> Router<AppState> {
     Router::new().route("/settings", routing::get(settings))
 }
 
+#[derive(Default)]
+struct Settings {
+    posts_per_page: u64,
+    keyboard_shortcuts: bool,
+    dark_theme: bool,
+    upscale_small_posts: bool,
+    endless_scroll: bool,
+    post_flow: bool,
+    transparency_grid: bool,
+    tag_suggestions: bool,
+    autoplay_videos: bool,
+    underscores_as_spaces: bool,
+}
+
 #[derive(Template)]
 #[template(path = "pages/settings.html")]
 struct SettingsTemplate {
     ctx: Context,
     active_tab: Tab,
+    settings: Settings,
 }
 
 async fn settings(ctx: Ctx) -> Html<String> {
+    let settings = Settings {
+        posts_per_page: 42,
+        keyboard_shortcuts: true,
+        transparency_grid: true,
+        tag_suggestions: true,
+        ..Default::default()
+    };
+
     let Ctx(ctx, _) = ctx;
     SettingsTemplate {
         ctx,
         active_tab: Tab::Settings,
+        settings,
     }
     .render()
     .map(Html)
