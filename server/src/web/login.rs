ebb2fb8ae9b1188746694ca60df5cf000a61fb8e
Liam Welch <liamw1@vt.edu>
2026-08-23 22:58:00 -0400

    wip: Implemented read-only password reset page.

--- server/src/web/login.rs
+++ server/src/web/login.rs
@@ -10,6 +10,25 @@ pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/login", routing::get(login))
         .route("/register", routing::get(register))
+        .route("/password-reset", routing::get(password_reset))
+}
+
+#[derive(Template)]
+#[template(path = "pages/registration.html")]
+struct RegistrationTemplate {
+    ctx: Context,
+    active_tab: Tab,
+}
+
+async fn register(ctx: Ctx) -> Html<String> {
+    let Ctx(ctx, _) = ctx;
+    RegistrationTemplate {
+        ctx,
+        active_tab: Tab::Account,
+    }
+    .render()
+    .map(Html)
+    .unwrap()
 }
 
 #[derive(Template)]
@@ -31,17 +50,17 @@ async fn login(ctx: Ctx) -> Html<String> {
 }
 
 #[derive(Template)]
-#[template(path = "pages/registration.html")]
-struct RegistrationTemplate {
+#[template(path = "pages/password_reset.html")]
+struct PasswordResetTemplate {
     ctx: Context,
     active_tab: Tab,
 }
 
-async fn register(ctx: Ctx) -> Html<String> {
+async fn password_reset(ctx: Ctx) -> Html<String> {
     let Ctx(ctx, _) = ctx;
-    RegistrationTemplate {
+    PasswordResetTemplate {
         ctx,
-        active_tab: Tab::Account,
+        active_tab: Tab::Login,
     }
     .render()
     .map(Html)
